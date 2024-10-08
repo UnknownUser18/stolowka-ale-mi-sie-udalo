228b700efd49d5411b29384d12fddd332706cf6c
UnknownUser18 <117106948+UnknownUser18@users.noreply.github.com>
2024-10-08 08:59:10 +0200

    Update script.js

--- frontend/panel_administracyjny/script.js
+++ frontend/panel_administracyjny/script.js
@@ -1,14 +1,16 @@
 const socket = new WebSocket("ws://localhost:8080");
-
 // Connection opened
 socket.addEventListener("open", (event) => {
-  socket.send("Hello Server!");
-
+    socket.send(JSON.stringify({
+        method: "CardScan",
+        id_ucznia: prompt("Id ucznia"),
+        timestamp: String(prompt("Time stamp"))
+    }))
 });
 
-// Listen for messages
+    // Listen for messages
 socket.addEventListener("message", (event) => {
-  console.log("Message from server ", event.data);
+    console.log("Message incoming from the server: ", event.data);
 });
 let eye = document.getElementById("eye");
 eye.addEventListener("click", () => {
