74eab51f27f442d7c553d84debdc47b4dd472e14
UnknownUser18 <unknownuser75@proton.me>
2024-10-26 18:07:00 +0200

    finished callendar

--- angularFrontend/panel_administracyjny/src/app/kalendarz/kalendarz.component.ts
+++ angularFrontend/panel_administracyjny/src/app/kalendarz/kalendarz.component.ts
@@ -1,27 +1,41 @@
-import { Component, ElementRef, Renderer2} from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {Component, ElementRef, Renderer2} from '@angular/core';
+import {NgForOf, NgOptimizedImage} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-kalendarz',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './kalendarz.component.html',
   styleUrl: './kalendarz.component.css'
 })
 export class KalendarzComponent {
+
   currentDate: string | undefined;
   date: Date = new Date();
   months: Array<string> = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
   month_before: string = this.months[new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1).getMonth()] + " " + new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1).getFullYear();
   month_next: string = this.months[new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1).getMonth()] + " " + new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1).getFullYear();
   selected: Array<string> = [];
+  posilki_cashe: Array<{id: number, array: Array<any>}> = [
+    {id: 1, array: []},
+    {id: 2, array: []},
+    {id: 3, array: []},
+    {id: 4, array: []},
+    {id: 5, array: []},
+    {id: 6, array: []},
+    {id: 7, array: []}
+  ];
 
   constructor(private renderer: Renderer2, private el: ElementRef) {
   };
 
   ngOnInit() {
+    this.previousPosilek = this.el.nativeElement.querySelector('#posilek').selectedIndex;
     this.show_calendar()
   }
 
@@ -39,7 +53,7 @@ export class KalendarzComponent {
     const weekDiv = this.renderer.createElement('div');
     this.renderer.addClass(weekDiv, 'week');
     this.renderer.appendChild(calendar_content, weekDiv);
-    for (let i = 1; i <= (month_days + first_day_week - 1); i++) {
+    for (let i = -7; i <= (month_days + first_day_week - 1); i++) {
       let week = this.el.nativeElement.getElementsByClassName('week')[weekcount];
       // create day button
       if (i < first_day_week) {
@@ -51,6 +65,9 @@ export class KalendarzComponent {
         const dayButton = this.renderer.createElement('button');
         this.renderer.addClass(dayButton, 'day');
         this.renderer.setProperty(dayButton, 'innerHTML', (i - first_day_week + 1).toString());
+        if (this.selected.includes(`${year}-${month+1}-${i - first_day_week + 1}`)) {
+          this.renderer.addClass(dayButton, 'selected');
+        }
         this.renderer.appendChild(week, dayButton);
       }
       if (i % 7 === 0) {
@@ -79,22 +96,52 @@ export class KalendarzComponent {
         }
       }
     });
+    // check if the week element is empty
+    Array.from(this.el.nativeElement.getElementsByClassName('week') as NodeListOf<HTMLElement>).forEach((week: HTMLElement) => {
+      let empty = true;
+      Array.from(week.children as unknown as NodeListOf<HTMLElement>).forEach((day: HTMLElement) => {
+        if (!day.classList.contains('empty')) {
+          empty = false;
+        }
+      });
+      if (empty) {
+        week.remove();
+      }
+    });
     // create zaznacz buttons
     const zaznacz: HTMLElement = this.el.nativeElement.querySelector('#zaznacz');
     zaznacz.innerHTML = '';
-    const week_length = this.el.nativeElement.getElementsByClassName('week').length;
+    let week_length = this.el.nativeElement.getElementsByClassName('week').length;
     for (let i = 0; i < week_length; i++) {
-      let zaznaczElement = this.renderer.createElement('button');
+      let selected_days = 0;
+      let days = 0;
+      Array.from(this.el.nativeElement.getElementsByClassName('week')[i].children as NodeListOf<HTMLElement>).forEach((day: HTMLElement) => {
+        if(!day.classList.contains('empty')) {
+          if(day.classList.contains('selected')) {
+            selected_days++;
+          }
+          days++;
+        }
+      });
+      let zaznaczLabel = this.renderer.createElement('label');
+      let zaznaczElement = this.renderer.createElement('input');
+      let zaznaczSpan = this.renderer.createElement('span');
       this.renderer.addClass(zaznaczElement, 'zaznacz');
-      this.renderer.setProperty(zaznaczElement, 'innerHTML', "Zaznacz");
-      this.renderer.appendChild(zaznacz, zaznaczElement);
+      this.renderer.setAttribute(zaznaczElement, 'type', 'checkbox');
+      // check if the entire array is selected
+      if(selected_days === days) {
+        console.log('selected');
+        this.renderer.setAttribute(zaznaczElement, 'checked', 'true');
+      }
+      this.renderer.appendChild(zaznaczLabel, zaznaczElement);
+      this.renderer.appendChild(zaznaczLabel, zaznaczSpan);
+      this.renderer.appendChild(zaznacz, zaznaczLabel);
     }
-  };
+  }
 
   // show previous/next month
   change_month(number: number) {
     this.date = new Date(this.date.getFullYear(), this.date.getMonth() + number, 1);
-    console.log(this.date)
     this.month_next = this.months[new Date(this.date.getFullYear(), this.date.getMonth() + 1, 1).getMonth()] + " " + new Date(this.date.getFullYear(), this.date.getMonth() + 1, 1).getFullYear()
     this.month_before = this.months[new Date(this.date.getFullYear(), this.date.getMonth() - 1, 1).getMonth()] + " " + new Date(this.date.getFullYear(), this.date.getMonth() - 1, 1).getFullYear()
     this.show_calendar();
@@ -104,12 +151,35 @@ export class KalendarzComponent {
     let target = element.target as HTMLElement;
     if (target.classList.contains('day')) {
       if (target.tagName === 'BUTTON') {
+        function isFullWeekSelected(week : HTMLElement) {
+          let selected = 0;
+          let days = 0;
+          Array.from(week.children as unknown as NodeListOf<HTMLElement>).forEach((day: HTMLElement) => {
+            if(!day.classList.contains('empty')) {
+              if(day.classList.contains('selected')) {
+                selected++;
+              }
+              days++;
+            }
+          });
+          return selected === days;
+        }
         if (target.classList.contains('selected')) {
+          if(isFullWeekSelected(target.parentElement as HTMLElement)) {
+            console.log('full week selected check = false');
+            const zaznacz = this.el.nativeElement.querySelector('#zaznacz > label > input');
+            (zaznacz as HTMLInputElement).checked = false;
+          }
           target.classList.remove('selected');
-          this.selected.splice(this.selected.indexOf(`${this.date.getFullYear()}-${this.date.getMonth()}-${parseInt(target.innerHTML)}`), 1);
+          this.selected.splice(this.selected.indexOf(`${this.date.getFullYear()}-${this.date.getMonth()+1}-${parseInt(target.innerHTML)}`), 1);
         } else {
           target.classList.add('selected');
-          this.selected.push(`${this.date.getFullYear()}-${this.date.getMonth()}-${parseInt(target.innerHTML)}`);
+          this.selected.push(`${this.date.getFullYear()}-${this.date.getMonth()+1}-${parseInt(target.innerHTML)}`);
+          if(isFullWeekSelected(target.parentElement as HTMLElement)) {
+            console.log('full week selected check = true');
+            const zaznacz = this.el.nativeElement.querySelector('#zaznacz > label > input');
+            (zaznacz as HTMLInputElement).checked = true;
+          }
         }
       }
     }
@@ -117,18 +187,63 @@ export class KalendarzComponent {
   }
   select_row(element : MouseEvent) {
     let target = element.target as HTMLElement;
-    if(target.tagName == "BUTTON") {
-      let parent = target.parentElement as HTMLElement; // żydon weś to spróbój skompresować
-      let children = parent.children;
-      let index = Array.from(children).indexOf(target);
-      const week = this.el.nativeElement.getElementsByClassName('week')[index];
-      for(let i = 0 ; i < week.children.length ; i ++) {
-        if(!(week.children[i].classList.contains('empty'))) {
-          const day = week.children[i];
-          console.log(day);
-          // skonczyć jutro
+    if(target.tagName === 'INPUT') {
+      const week = this.el.nativeElement.getElementsByClassName('week')[Array.from(this.el.nativeElement.querySelectorAll('.zaznacz')).indexOf(target)];
+      if((target as HTMLInputElement).checked) {
+        console.log('checked');
+        for(let i = 0; i < week.children.length; i++) {
+          if(!week.children[i].classList.contains('empty')) {
+            week.children[i].classList.add('selected');
+            this.selected.push(`${this.date.getFullYear()}-${this.date.getMonth()+1}-${parseInt(week.children[i].innerHTML)}`);
+          }
         }
       }
+      else {
+        console.log('unchecked');
+        for(let i = 0; i < week.children.length; i++) {
+          if(!week.children[i].classList.contains('empty')) {
+            week.children[i].classList.remove('selected');
+            this.selected.splice(this.selected.indexOf(`${this.date.getFullYear()}-${this.date.getMonth()+1}-${parseInt(week.children[i].innerHTML)}`), 1);
+          }
+        }
+      }
+      console.log(this.selected);
     }
+  }
+  // posilki
+  GetPosilki(posilek: {id: number, nazwa: string}): string {
+    const rename: { [key : number] : string} = {
+      0: 'Śniadanie',
+      1: 'Obiad',
+      2: 'Kolacja',
+      3: 'Śniadanie i Obiad',
+      4: 'Śniadanie i Kolacja',
+      5: 'Śniadanie, Obiad i Kolacja',
+      6: 'Obiad i Kolacja'
+    }
+    return rename[posilek.id] || posilek.nazwa;
+  }
+  posilki = [
+    {id : 0, nazwa: "śniadanie"},
+    {id : 1, nazwa: "obiad"},
+    {id : 2, nazwa: "kolacja"},
+    {id : 3, nazwa: "śniadanie_obiad"},
+    {id : 4, nazwa: "śniadanie_kolacja"},
+    {id : 5, nazwa: "śniadanie_obiad_kolacja"},
+    {id : 6, nazwa: "obiad_kolacja"},
+  ]
+  selectedPosilek: number = this.posilki[1].id;
+  previousPosilek : number | undefined;
+
+  select_posilek() {
+    console.log(this.previousPosilek)
+    // @ts-ignore
+    this.posilki_cashe[this.previousPosilek].array = this.selected;
+    this.previousPosilek = this.selectedPosilek;
+    console.log(this.previousPosilek)
+    this.selected = this.posilki_cashe[this.selectedPosilek].array;
+    console.log(this.selected);
+    console.log(this.posilki_cashe);
+    this.show_calendar();
   }
 }
