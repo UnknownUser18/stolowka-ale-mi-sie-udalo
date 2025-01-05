6c1193026ea67f5286d2a997d3d984d8acad6724
UnknownUser18 <unknownuser75@proton.me>
2025-01-05 17:45:09 +0100

    Codacy Fixes 3.

--- angularFrontend/panel_administracyjny/src/app/app.component.ts
+++ angularFrontend/panel_administracyjny/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import {Component, OnInit, ElementRef} from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { PanelComponent } from './panel/panel.component';
-import {NgForOf, NgOptimizedImage} from '@angular/common';
-// @ts-ignore
-import {DataBaseService} from './data-base.service';
-import {GlobalnyPanelComponent} from './globalny-panel/globalny-panel.component';
+import { NgForOf, NgOptimizedImage } from '@angular/common';
+import { DataBaseService } from './data-base.service';
+import { GlobalnyPanelComponent } from './globalny-panel/globalny-panel.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UnsavedChangesDialogComponent } from './unsaved-changes-dialog/unsaved-changes-dialog.component';
 
@@ -15,55 +14,40 @@ import { UnsavedChangesDialogComponent } from './unsaved-changes-dialog/unsaved-
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  DOMelement : any | undefined;
+  DOMelement: any;
+  sorted_zsti_users: Array<{ imie: string, nazwisko: string }> = [];
+  sorted_internat_users: Array<{ imie: string, nazwisko: string }> = [];
+  StudentListZstiData: Array<{ id: number, imie: string, nazwisko: string }> | null = null;
+  StudentListInternatData: Array<{ id: number, imie: string, nazwisko: string }> | null = null;
+  osoba: string | undefined;
+  title: string = 'panel_administracyjny';
+  typ: string | undefined;
 
-  zsti_users: Array<{imie: string[], nazwisko: string[]}> = [
-  {
-    imie: [],
-    nazwisko: []
+  constructor(private dataService: DataBaseService, private el: ElementRef, private dialog: MatDialog) {
+    this.DOMelement = this.el.nativeElement;
   }
-  ];
-  internat_users : Array<{imie: string[], nazwisko: string[]}> = [
-  {
-    imie: [],
-    nazwisko: []
-  }
-  ];
-  sorted_zsti_users: Array<{ imie : string, nazwisko : string}> = [];
-  sorted_internat_users: Array<{ imie: string, nazwisko : string}> = [];
-  StudentListZstiData : Array<{ id : number, imie : string, nazwisko : string}> | null = null;
-  StudentListInternatData: Array<{ id : number, imie : string, nazwisko : string }> | null = null;
-  constructor(private dataService:DataBaseService, private el: ElementRef, private dialog: MatDialog) {
-   this.DOMelement = this.el.nativeElement ? this.el.nativeElement : undefined;
-  }
+
   ngOnInit() {
-  this.dataService.StudentListZstiData.subscribe((data: Array<{ id : number, imie : string, nazwisko : string }>) => {
-    this.StudentListZstiData = data;
-    if (this.StudentListZstiData != null) {
-      this.zsti_users[0].imie = this.StudentListZstiData.map((element) => element.imie);
-      this.zsti_users[0].nazwisko = this.StudentListZstiData.map((element) => element.nazwisko);
-      let zsti_users_flat: Array<{ imie: string, nazwisko: string }> = this.zsti_users[0].imie.map((imie, index) => {
-        return { imie, nazwisko: this.zsti_users[0].nazwisko[index] };
-      });
-      zsti_users_flat.sort((a, b) => a.nazwisko.localeCompare(b.nazwisko));
-      this.sorted_zsti_users = zsti_users_flat;
-    }
-  });
-
-  this.dataService.StudentListInternatData.subscribe((data: any) => {
-    this.StudentListInternatData = data;
-    if (this.StudentListInternatData != null) {
-      this.internat_users[0].imie = this.StudentListInternatData.map((element: any) => element.imie);
-      this.internat_users[0].nazwisko = this.StudentListInternatData.map((element: any) => element.nazwisko);
-      let internat_users_flat: Array<{ imie: string, nazwisko: string }> = this.internat_users[0].imie.map((imie, index) => {
-        return { imie, nazwisko: this.internat_users[0].nazwisko[index] };
-      });
-      internat_users_flat.sort((a, b) => a.nazwisko.localeCompare(b.nazwisko));
-      this.sorted_internat_users = internat_users_flat;
-    }
-});
+    this.dataService.StudentListZstiData.subscribe(data => this.updateUserList(data, 'ZSTI'));
+    this.dataService.StudentListInternatData.subscribe(data => this.updateUserList(data, 'Internat'));
   }
-  szukaj() : void {
+
+  updateUserList(data: Array<{ id: number, imie: string, nazwisko: string }>, type: string) {
+    if (type === 'ZSTI') {
+      this.StudentListZstiData = data;
+      this.sorted_zsti_users = this.sortUsers(data);
+    } else {
+      this.StudentListInternatData = data;
+      this.sorted_internat_users = this.sortUsers(data);
+    }
+  }
+
+  sortUsers(data: Array<{ id: number, imie: string, nazwisko: string }>) {
+    return data.map(user => ({ imie: user.imie, nazwisko: user.nazwisko }))
+               .sort((a, b) => a.nazwisko.localeCompare(b.nazwisko));
+  }
+
+  szukaj(): void {
     const searchTerm = this.el.nativeElement.querySelector('#wyszukaj > input').value.toLowerCase();
     const sections = this.DOMelement.querySelectorAll('section > ol > li');
     sections.forEach((element: HTMLElement) => {
@@ -74,46 +58,58 @@ export class AppComponent implements OnInit {
         element.style.display = 'block';
       });
     } else {
-      this.rozwin(new Event('click'));
-      this.rozwin(new Event('click'));
+      this.toggleSections();
     }
-}
-  cantDoThat(func:Function) {
-    const dialogRef = this.dialog?.open(UnsavedChangesDialogComponent, {
-      width: '400px',
-    });
-    dialogRef.afterClosed().subscribe((result) => {
+  }
+
+  toggleSections() {
+    this.rozwin(new Event('click'));
+    this.rozwin(new Event('click'));
+  }
+
+  cantDoThat(func: Function) {
+    const dialogRef = this.dialog.open(UnsavedChangesDialogComponent, { width: '400px' });
+    dialogRef.afterClosed().subscribe(result => {
       if (result === 'proceed') {
-        this.dataService.SavedList.forEach((element:any) => {
-          element.next(true)
-        })
+        this.dataService.SavedList.forEach(element => element.next(true));
         func();
       }
     });
   }
 
-  osoba : string | undefined;
-  title: string = 'panel_administracyjny';
-  typ : string | undefined;
   show(event: Event): void {
     if (this.dataService.SavedList.some(element => !element.value)) {
       this.cantDoThat(() => this.show(event));
       return;
     }
-    let target = event.target as HTMLElement;
+    let target = this.getEventTarget(event);
     if (target.tagName === 'OL') return;
+    this.displayPanels(target);
+    this.updateStudentData(target);
+  }
+
+  getEventTarget(event: Event): HTMLElement {
+    let target = event.target as HTMLElement;
     if (target.tagName === 'SPAN') {
       target = target.parentElement as HTMLElement;
     }
+    return target;
+  }
+
+  displayPanels(target: HTMLElement) {
     this.DOMelement.querySelector('app-globalny-panel').style.display = 'none';
     this.DOMelement.querySelector('app-panel').style.display = 'block';
     this.osoba = target.querySelector('span')?.textContent!;
+  }
+
+  updateStudentData(target: HTMLElement) {
     const index = parseInt(target.getAttribute('data-index')!, 10);
     const studentData = this.typ === 'ZSTI' ? this.StudentListZstiData : this.StudentListInternatData;
     if (studentData && studentData[index] && this.typ) {
       this.dataService.changeStudent(studentData[index].id, this.typ);
     }
   }
+
   rozwin(event: Event): void {
     let target = event.target as HTMLElement;
     while (target.tagName !== 'BUTTON') {
@@ -125,8 +121,8 @@ export class AppComponent implements OnInit {
     ol.classList.toggle('show');
     img.classList.toggle('rotate');
   }
-  protected readonly JSON : JSON = JSON;
-  main_menu() : void {
+
+  main_menu(): void {
     this.DOMelement.querySelector('app-globalny-panel').style.display = 'block';
     this.DOMelement.querySelector('app-panel').style.display = 'none';
   }
