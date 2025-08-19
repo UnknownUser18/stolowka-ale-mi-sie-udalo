b4cd39edae7b423653aa68a800927a7433daecd0
UnknownUser18 <unknownuser75@proton.me>
2025-08-19 11:08:12 +0200

    Rearranged Angular routing

--- frontend/panel_administracyjny/src/app/app.routes.ts
+++ frontend/panel_administracyjny/src/app/app.routes.ts
@@ -1,60 +1,91 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CennikComponent } from './cennik/cennik.component';
-import { NieczynneComponent } from './nieczynne/nieczynne.component';
-import { ZstiComponent } from './users/zsti/zsti.component';
-import { KalendarzComponent } from './users/kalendarz/kalendarz.component';
-import { DaneComponent } from './users/dane/dane.component';
-import { DeklaracjeComponent } from './users/deklaracje/deklaracje.component';
-import { PlatnosciComponent } from './users/platnosci/platnosci.component';
-import { KartyComponent } from './users/karty/karty.component';
-import { RaportsComponent } from './raports/raports.component';
 import { LoginComponent } from './login/login.component';
-import { IndividualRaportComponent } from './users/individual-raport/individual-raport.component';
+import { mainGuard } from './guards/main.guard';
+import { NieczynneComponent } from './nieczynne/nieczynne.component';
+import { RaportsComponent } from './raports/raports.component';
 import { AdministracjaOsobComponent } from './administracja-osob/administracja-osob.component';
+import { CennikComponent } from './cennik/cennik.component';
+import { ZstiComponent } from './users/zsti/zsti.component';
 
 export const routes : Routes = [
-  { path : 'api', redirectTo : '', pathMatch : 'full' },
-  { path : '', component : HomeComponent },
   {
-    path : 'osoby',
-    component : ZstiComponent,
+    path : '', component : HomeComponent,
+    canActivateChild : [mainGuard],
     children : [
-      { path : 'zsti', component : ZstiComponent },
-    ],
-  },
-  { path : 'osoby/zsti', component : ZstiComponent },
-  { path : 'osoba/zsti/:id/kalendarz', component : KalendarzComponent },
-  { path : 'osoba/zsti/:id/dane', component : DaneComponent },
-  { path : 'osoba/zsti/:id/deklaracje', component : DeklaracjeComponent },
-  { path : 'osoba/zsti/:id/platnosci', component : PlatnosciComponent },
-  { path : 'osoba/zsti/:id/karta', component : KartyComponent },
-  { path : 'osoba/zsti/:id/raport', component : IndividualRaportComponent },
-  { path : 'cennik', component : CennikComponent },
-  { path : 'cennik/zsti', component : CennikComponent },
-  { path : 'nieczynne', component : NieczynneComponent },
-  { path : 'nieczynne/zsti', component : NieczynneComponent },
-  { path : 'osoba/zsti/:id/karta', component : KartyComponent },
-  {
-    path : 'raporty', component : RaportsComponent,
-    children : [
-      { path : 'korekty', component : RaportsComponent },
-      { path : 'obecnosci', component : RaportsComponent },
-      { path : 'platnosci', component : RaportsComponent },
+      {
+        path : 'osoby',
+        loadComponent : () => import('./users/osoby/osoby.component').then(m => m.OsobyComponent),
+        children : [
+          { path : 'zsti', component : ZstiComponent, },
+          {
+            path : 'internat' // TODO: Add internat component
+          }
+        ],
+      },
+      {
+        path : 'osoba',
+        children : [
+          {
+            path : 'zsti/:id',
+            children : [
+              { path : '', redirectTo : 'kalendarz', pathMatch : 'full' },
+              ...['kalendarz', 'dane', 'deklaracje', 'platnosci', 'karta', 'raport'].map(childPath => ({
+                path : childPath,
+                loadComponent : () => import(`./users/${ childPath }/${ childPath }.component`).then(m => m[`${ childPath.charAt(0).toUpperCase() + childPath.slice(1) }Component`])
+              }))
+            ]
+          },
+          {
+            path : 'internat/:id', // TODO: Add internat component
+            children : [
+              { path : '', redirectTo : 'kalendarz', pathMatch : 'full' },
+              ...['kalendarz', 'dane', 'deklaracje', 'platnosci', 'karta', 'raport'].map(childPath => ({
+                path : childPath,
+                loadComponent : () => import(`./users/${ childPath }/${ childPath }.component`).then(m => m[`${ childPath.charAt(0).toUpperCase() + childPath.slice(1) }Component`])
+              }))
+            ]
+          }
+        ]
+      },
+      {
+        path : 'cennik',
+        loadComponent : () => import('./cennik/cennik.component').then(m => m.CennikComponent),
+        children : [
+          { path : 'zsti', component : CennikComponent },
+        ]
+      },
+      {
+        path : 'nieczynne',
+        loadComponent : () => import('./nieczynne/nieczynne.component').then(m => m.NieczynneComponent),
+        children : [
+          { path : 'zsti', component : NieczynneComponent },
+        ]
+      },
+      {
+        path : 'raporty',
+        loadComponent : () => import('./raports/raports.component').then(m => m.RaportsComponent),
+        children : [
+          { path : 'korekty', component : RaportsComponent },
+          { path : 'obecnosci', component : RaportsComponent },
+          { path : 'platnosci', component : RaportsComponent },
+        ]
+      },
+      {
+        path : 'administracja',
+        loadComponent : () => import('./administracja-osob/administracja-osob.component').then(m => m.AdministracjaOsobComponent),
+        children : [
+          { path : 'users', component : AdministracjaOsobComponent },
+          { path : 'klasy', component : AdministracjaOsobComponent },
+          { path : 'dodaj-osobe', component : AdministracjaOsobComponent },
+        ]
+      }
     ]
   },
-  { path : 'login', component : LoginComponent },
-  {
-    path : 'administracja', component : AdministracjaOsobComponent,
-    children : [
-      { path : 'users', component : AdministracjaOsobComponent },
-      { path : 'klasy', component : AdministracjaOsobComponent },
-      { path : 'dodaj-osobe', component : AdministracjaOsobComponent },
-    ]
 
-  },
-  { path : '**', redirectTo : 'login' } // Redirect to log in for any unknown routes
+  { path : 'login', component : LoginComponent },
+  { path : '**', redirectTo : '' }
 ];
 
 @NgModule({
---
+++ frontend/panel_administracyjny/src/app/users/osoby/osoby.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-osoby',
+  imports: [],
+  templateUrl: './osoby.component.html',
+  styleUrl: './osoby.component.scss'
+})
+export class OsobyComponent {
+
+}
